91592f50a161f5d32adcd935615c36423c9ed1c8
Alexey Grazhdankin <alex.alexxx56@gmail.com>
2023-06-03 01:15:26 +0300

    Добавлены докстринги для классов и их методов в файле engine_classes.py

--- engine_classes.py
+++ engine_classes.py
@@ -3,8 +3,11 @@ from abstract_class import APIEngine
 import requests
 import os
 
-class HeadHunter(APIEngine):
 
+class HeadHunter(APIEngine):
+    """
+    Класс для работы с API HeadHunter
+    """
     def __init__(self, keyword: str, page=0):
         self.url = "https://api.hh.ru/vacancies"
         self.params = {
@@ -13,10 +16,16 @@ class HeadHunter(APIEngine):
         }
 
     def get_request(self):
+        """
+        Метод осуществляет поиск по API HeadHunter в соответствии с ключевым словом
+        """
         print(requests.get(self.url, params=self.params).json()["items"])
         return requests.get(self.url, params=self.params).json()["items"]
 
     def get_vacancies(self):
+        """
+        Метод создает экземпляры класса Vacancy
+        """
         for vacancy_data in self.get_request():
             Vacancy(vacancy_data["name"],
                     vacancy_data["alternate_url"],
@@ -27,20 +36,28 @@ class HeadHunter(APIEngine):
 
 
 class SuperJob(APIEngine):
+    """Класс для работы с API SuperJob"""
     def __init__(self, keyword: str, page=1):
         self.url = "https://api.superjob.ru/2.0/vacancies"
         self.params = {
             "keywords": keyword,
             "page": page,
+            "area": 113,
         }
 
     def get_request(self):
+        """
+        Метод осуществляет поиск по API SuperJob в соответствии с ключевым словом
+        """
         headers = {'X-Api-App-Id': os.environ['X-Api-App-Id']}
         ret = requests.get(self.url, headers=headers, params=self.params).json()["objects"]
         print(ret)
         return ret
 
     def get_vacancies(self):
+        """
+        Метод создает экземпляры класса Vacancy
+        """
         for vacancy_data in self.get_request():
             Vacancy(vacancy_data["profession"],
                     vacancy_data["link"],
