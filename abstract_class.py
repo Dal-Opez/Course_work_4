0d14dd5bee79af7e29a7155bb4a7ccdba62af345
Alexey Grazhdankin <alex.alexxx56@gmail.com>
2023-06-03 00:38:54 +0300

    Добавлен абстрактный класс VacancySaver

--- abstract_class.py
+++ abstract_class.py
@@ -10,3 +10,17 @@ class APIEngine(ABC):
     @abstractmethod
     def get_vacancies(self):
         pass
+
+
+class VacancySaver(ABC):
+    @abstractmethod
+    def add_vacancy(self):
+        pass
+
+    @abstractmethod
+    def select_vacancies(self):
+        pass
+
+    @abstractmethod
+    def delete_vacancies(self):
+        pass
