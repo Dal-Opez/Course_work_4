61b56cd022b40a61904c90fa521b0fef5e7753ef
Alexey Grazhdankin <alex.alexxx56@gmail.com>
2023-06-03 00:34:27 +0300

    Добавлен класс SuperJob

--- engine_classes.py
+++ engine_classes.py
@@ -24,3 +24,27 @@ class HeadHunter(APIEngine):
                     vacancy_data["salary"]["from"] if vacancy_data.get("salary") else None,
                     vacancy_data["salary"]["to"] if vacancy_data.get("salary") else None,
                     vacancy_data["snippet"]["requirement"])
+
+
+class SuperJob(APIEngine):
+    def __init__(self, keyword: str, page=1):
+        self.url = "https://api.superjob.ru/2.0/vacancies"
+        self.params = {
+            "keywords": keyword,
+            "page": page,
+        }
+
+    def get_request(self):
+        headers = {'X-Api-App-Id': os.environ['X-Api-App-Id']}
+        ret = requests.get(self.url, headers=headers, params=self.params).json()["objects"]
+        print(ret)
+        return ret
+
+    def get_vacancies(self):
+        for vacancy_data in self.get_request():
+            Vacancy(vacancy_data["profession"],
+                    vacancy_data["link"],
+                    vacancy_data["currency"].upper(),
+                    vacancy_data["payment_from"],
+                    vacancy_data["payment_to"],
+                    vacancy_data["type_of_work"]["title"])
